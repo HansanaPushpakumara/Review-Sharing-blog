82eff0be4f2a3a2c8620655d5debdf07bc743f25
Hansana Pushpakumara <43991241+HansanaPushpakumara@users.noreply.github.com>
2020-06-23 16:26:54 +0530

    first commit

---
+++ application/logs/log-2020-01-20.php
@@ -0,0 +1,124 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2020-01-20 08:43:17 --> Config Class Initialized
+INFO - 2020-01-20 08:43:17 --> Hooks Class Initialized
+DEBUG - 2020-01-20 08:43:17 --> UTF-8 Support Enabled
+INFO - 2020-01-20 08:43:17 --> Utf8 Class Initialized
+INFO - 2020-01-20 08:43:17 --> URI Class Initialized
+INFO - 2020-01-20 08:43:17 --> Router Class Initialized
+INFO - 2020-01-20 08:43:17 --> Output Class Initialized
+INFO - 2020-01-20 08:43:17 --> Security Class Initialized
+DEBUG - 2020-01-20 08:43:17 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-01-20 08:43:17 --> Input Class Initialized
+INFO - 2020-01-20 08:43:17 --> Language Class Initialized
+INFO - 2020-01-20 08:43:17 --> Loader Class Initialized
+INFO - 2020-01-20 08:43:17 --> Helper loaded: url_helper
+INFO - 2020-01-20 08:43:17 --> Helper loaded: form_helper
+INFO - 2020-01-20 08:43:17 --> Helper loaded: text_helper
+INFO - 2020-01-20 08:43:17 --> Helper loaded: file_helper
+INFO - 2020-01-20 08:43:17 --> Database Driver Class Initialized
+INFO - 2020-01-20 08:43:17 --> Form Validation Class Initialized
+DEBUG - 2020-01-20 08:43:17 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-01-20 08:43:17 --> Session: Class initialized using 'files' driver.
+INFO - 2020-01-20 08:43:17 --> Model "User_model" initialized
+INFO - 2020-01-20 08:43:17 --> Model "Post_model" initialized
+INFO - 2020-01-20 08:43:17 --> Model "Review_model" initialized
+INFO - 2020-01-20 08:43:17 --> Controller Class Initialized
+INFO - 2020-01-20 08:43:17 --> File loaded: C:\wamp64\www\project\application\views\includes/header.php
+INFO - 2020-01-20 08:43:17 --> File loaded: C:\wamp64\www\project\application\views\includes/messages.php
+INFO - 2020-01-20 08:43:17 --> File loaded: C:\wamp64\www\project\application\views\writer/index.php
+INFO - 2020-01-20 08:43:17 --> File loaded: C:\wamp64\www\project\application\views\includes/footer.php
+INFO - 2020-01-20 08:43:17 --> Final output sent to browser
+DEBUG - 2020-01-20 08:43:17 --> Total execution time: 0.5430
+INFO - 2020-01-20 08:43:18 --> Config Class Initialized
+INFO - 2020-01-20 08:43:18 --> Hooks Class Initialized
+DEBUG - 2020-01-20 08:43:18 --> UTF-8 Support Enabled
+INFO - 2020-01-20 08:43:18 --> Utf8 Class Initialized
+INFO - 2020-01-20 08:43:18 --> URI Class Initialized
+INFO - 2020-01-20 08:43:18 --> Router Class Initialized
+INFO - 2020-01-20 08:43:19 --> Output Class Initialized
+INFO - 2020-01-20 08:43:19 --> Security Class Initialized
+DEBUG - 2020-01-20 08:43:19 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-01-20 08:43:19 --> Input Class Initialized
+INFO - 2020-01-20 08:43:19 --> Language Class Initialized
+INFO - 2020-01-20 08:43:19 --> Loader Class Initialized
+INFO - 2020-01-20 08:43:19 --> Helper loaded: url_helper
+INFO - 2020-01-20 08:43:19 --> Helper loaded: form_helper
+INFO - 2020-01-20 08:43:19 --> Helper loaded: text_helper
+INFO - 2020-01-20 08:43:19 --> Helper loaded: file_helper
+INFO - 2020-01-20 08:43:19 --> Database Driver Class Initialized
+INFO - 2020-01-20 08:43:19 --> Form Validation Class Initialized
+DEBUG - 2020-01-20 08:43:19 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-01-20 08:43:19 --> Session: Class initialized using 'files' driver.
+INFO - 2020-01-20 08:43:19 --> Model "User_model" initialized
+INFO - 2020-01-20 08:43:19 --> Model "Post_model" initialized
+INFO - 2020-01-20 08:43:19 --> Model "Review_model" initialized
+INFO - 2020-01-20 08:43:19 --> Controller Class Initialized
+INFO - 2020-01-20 08:43:19 --> File loaded: C:\wamp64\www\project\application\views\includes/header.php
+INFO - 2020-01-20 08:43:19 --> File loaded: C:\wamp64\www\project\application\views\includes/messages.php
+INFO - 2020-01-20 08:43:19 --> File loaded: C:\wamp64\www\project\application\views\writer/create_post.php
+INFO - 2020-01-20 08:43:19 --> File loaded: C:\wamp64\www\project\application\views\includes/footer.php
+INFO - 2020-01-20 08:43:19 --> Final output sent to browser
+DEBUG - 2020-01-20 08:43:19 --> Total execution time: 0.2996
+INFO - 2020-01-20 08:43:27 --> Config Class Initialized
+INFO - 2020-01-20 08:43:27 --> Hooks Class Initialized
+DEBUG - 2020-01-20 08:43:27 --> UTF-8 Support Enabled
+INFO - 2020-01-20 08:43:27 --> Utf8 Class Initialized
+INFO - 2020-01-20 08:43:27 --> URI Class Initialized
+INFO - 2020-01-20 08:43:27 --> Router Class Initialized
+INFO - 2020-01-20 08:43:27 --> Output Class Initialized
+INFO - 2020-01-20 08:43:27 --> Security Class Initialized
+DEBUG - 2020-01-20 08:43:27 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-01-20 08:43:27 --> Input Class Initialized
+INFO - 2020-01-20 08:43:27 --> Language Class Initialized
+INFO - 2020-01-20 08:43:27 --> Loader Class Initialized
+INFO - 2020-01-20 08:43:27 --> Helper loaded: url_helper
+INFO - 2020-01-20 08:43:27 --> Helper loaded: form_helper
+INFO - 2020-01-20 08:43:27 --> Helper loaded: text_helper
+INFO - 2020-01-20 08:43:27 --> Helper loaded: file_helper
+INFO - 2020-01-20 08:43:27 --> Database Driver Class Initialized
+INFO - 2020-01-20 08:43:27 --> Form Validation Class Initialized
+DEBUG - 2020-01-20 08:43:27 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-01-20 08:43:27 --> Session: Class initialized using 'files' driver.
+INFO - 2020-01-20 08:43:27 --> Model "User_model" initialized
+INFO - 2020-01-20 08:43:27 --> Model "Post_model" initialized
+INFO - 2020-01-20 08:43:27 --> Model "Review_model" initialized
+INFO - 2020-01-20 08:43:27 --> Controller Class Initialized
+INFO - 2020-01-20 08:43:27 --> Language file loaded: language/english/form_validation_lang.php
+INFO - 2020-01-20 08:43:27 --> Upload Class Initialized
+ERROR - 2020-01-20 08:43:27 --> Severity: Warning --> Illegal offset type in isset or empty C:\wamp64\www\project\system\libraries\Upload.php 377
+ERROR - 2020-01-20 08:43:27 --> Severity: Warning --> preg_match_all() expects parameter 2 to be string, array given C:\wamp64\www\project\system\libraries\Upload.php 382
+INFO - 2020-01-20 08:43:27 --> Language file loaded: language/english/upload_lang.php
+DEBUG - 2020-01-20 08:43:27 --> You did not select a file to upload.
+DEBUG - 2020-01-20 08:43:27 --> You have not specified any allowed file types.
+DEBUG - 2020-01-20 08:43:27 --> The filetype you are attempting to upload is not allowed.
+INFO - 2020-01-20 08:43:27 --> Config Class Initialized
+INFO - 2020-01-20 08:43:27 --> Hooks Class Initialized
+DEBUG - 2020-01-20 08:43:27 --> UTF-8 Support Enabled
+INFO - 2020-01-20 08:43:27 --> Utf8 Class Initialized
+INFO - 2020-01-20 08:43:27 --> URI Class Initialized
+INFO - 2020-01-20 08:43:27 --> Router Class Initialized
+INFO - 2020-01-20 08:43:27 --> Output Class Initialized
+INFO - 2020-01-20 08:43:27 --> Security Class Initialized
+DEBUG - 2020-01-20 08:43:27 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-01-20 08:43:27 --> Input Class Initialized
+INFO - 2020-01-20 08:43:27 --> Language Class Initialized
+INFO - 2020-01-20 08:43:27 --> Loader Class Initialized
+INFO - 2020-01-20 08:43:27 --> Helper loaded: url_helper
+INFO - 2020-01-20 08:43:27 --> Helper loaded: form_helper
+INFO - 2020-01-20 08:43:27 --> Helper loaded: text_helper
+INFO - 2020-01-20 08:43:27 --> Helper loaded: file_helper
+INFO - 2020-01-20 08:43:27 --> Database Driver Class Initialized
+INFO - 2020-01-20 08:43:27 --> Form Validation Class Initialized
+DEBUG - 2020-01-20 08:43:27 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-01-20 08:43:27 --> Session: Class initialized using 'files' driver.
+INFO - 2020-01-20 08:43:28 --> Model "User_model" initialized
+INFO - 2020-01-20 08:43:28 --> Model "Post_model" initialized
+INFO - 2020-01-20 08:43:28 --> Model "Review_model" initialized
+INFO - 2020-01-20 08:43:28 --> Controller Class Initialized
+INFO - 2020-01-20 08:43:28 --> File loaded: C:\wamp64\www\project\application\views\includes/header.php
+INFO - 2020-01-20 08:43:28 --> File loaded: C:\wamp64\www\project\application\views\includes/messages.php
+INFO - 2020-01-20 08:43:28 --> File loaded: C:\wamp64\www\project\application\views\writer/index.php
+INFO - 2020-01-20 08:43:28 --> File loaded: C:\wamp64\www\project\application\views\includes/footer.php
+INFO - 2020-01-20 08:43:28 --> Final output sent to browser
+DEBUG - 2020-01-20 08:43:28 --> Total execution time: 0.2842
